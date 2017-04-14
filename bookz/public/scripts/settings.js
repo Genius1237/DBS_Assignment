479dede417c318e85842c737483a79846e64a55f
ajay0 <ajaykrishnak0@gmail.com>
2017-04-15 02:26:31 +0530

    Add settings page

---
+++ bookz/public/scripts/settings.js
@@ -0,0 +1,98 @@
+var defaultAlertBoxValue = {
+  name: 'Name chars',
+  username: 'Not editable',
+  password: 'Password chars',
+  phone: 'Phone chars'
+}
+
+function onFocus(event) {
+  // make changes to alert box
+  var alertElement = document.getElementById(event.target.name + '-alert');
+  alertElement.textContent = defaultAlertBoxValue[event.target.name];
+  if (event.target.name === 'username')
+    alertElement.style.backgroundColor = '#444744';
+  else {
+    alertElement.style.backgroundColor = '#00b300';
+  }
+  alertElement.style.opacity = '1';
+}
+
+function onBlur(event) {
+  // make changes to alert box
+  var alertElement = document.getElementById(event.target.name + '-alert').style.opacity = '0';
+}
+
+function onMouseOver(event) {
+  event.target.style.boxShadow = "0px 0px 2px 1px black";
+}
+
+function onMouseOut(event) {
+  event.target.style.boxShadow = "";
+}
+
+function onClick(event) {
+  // prepare payload for AJAX request
+  var payload = "";
+  var els = document.getElementsByTagName('input');
+  for (let el of els) {
+    payload += el.name + "=" + el.value + "&";
+  }
+  payload = payload.slice(0, payload.length - 1);
+
+  // modify request status element
+  var requestStatusElement = document.getElementById('request-status')
+  requestStatusElement.style.backgroundColor = '#ff001b';
+  requestStatusElement.style.textContent = 'Submitting..';
+  requestStatusElement.style.visibility = 'visible';
+
+  // make AJAX request
+  var xhr = new XMLHttpRequest();
+  xhr.open('PUT', 'http://localhost:3000/users');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.send(payload);
+
+  // handle response to AJAX request
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === xhr.DONE) {
+      if (xhr.status === 200) {
+        // assuming response is in JSON
+        let response = JSON.parse(xhr.response);
+        if (response.valid === 'true') {
+          requestStatusElement.style.visibility = 'visible';
+          requestStatusElement.style.backgroundColor = '#00b300';
+          requestStatusElement.style.textContent = 'Updated successfully';
+        } else {
+          // highlight those fields that are invalid
+          for (let name of response.invalidFields) {
+            let el = document.getElementById(name + '-alert');
+            el.style.backgroundColor = '#ff001b';
+            el.textContent = 'Invalid value entered';
+            el.style.opacity = '1';
+          }
+        }
+      } else {
+        requestStatusElement.style.textContent = 'Required response not received';
+      }
+    }
+  }
+}
+
+function initialise() {
+
+  // add listeners to input fields to respond to focus and blur
+  var els = document.getElementsByTagName('input');
+  for (let el of els) {
+      el.addEventListener('focus', onFocus);
+      el.addEventListener('blur', onBlur);
+  }
+
+  // add listeners to submit button to respond to hover
+  var submitButton = document.getElementById('update-button');
+  submitButton.addEventListener('mouseover', onMouseOver);
+  submitButton.addEventListener('mouseout', onMouseOut);
+
+  // add listeners to form to respond to submit
+  document.getElementById('update-button').addEventListener('click', onClick);
+}
+
+document.addEventListener('DOMContentLoaded', initialise);
